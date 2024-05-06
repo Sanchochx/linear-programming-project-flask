a522bf8428c163de6cb07a6d0d0ec3d87d64fe4e
Sanchochx <santiagosanchezmarquez@gmail.com>
2024-05-06 10:51:47 -0500

    optimization project

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def home():
 def calculadora():
     return render_template("index.html")
 
+@app.route("/planteamiento")
+def planteamiento():
+    return render_template("planteamiento.html")
+
 @app.route("/solve", methods=['POST'])
 def solve():
     maximize = True if request.form.get('optimization_type') == 'maximizar' else False
@@ -22,17 +26,13 @@ def solve():
     relation_operators = [request.form.get(f'relation_{i}') for i in range(3)]
     constraint_constants = [float(request.form.get(f'constant_{i}')) for i in range(3)]
 
-    # Definir problema
     problem = pulp.LpProblem("Problema Programación Lineal", pulp.LpMaximize if maximize else pulp.LpMinimize)
 
-    # Definir variables
     decision_vars = [pulp.LpVariable(f'x{i}', lowBound=0) for i in range(1, 4)]
 
-    # Construir funcion objetivo
     objective = pulp.lpSum([coef * var for coef, var in zip(objective_coefficients, decision_vars)])
     problem += objective
 
-    # Constantes
     for i, constraint_coeffs in enumerate(constraint_entries_list):
         if relation_operators[i] == '<=':
             problem += pulp.lpSum([coef * var for coef, var in zip(constraint_coeffs, decision_vars)]) <= constraint_constants[i]
@@ -41,10 +41,8 @@ def solve():
         elif relation_operators[i] == '=':
             problem += pulp.lpSum([coef * var for coef, var in zip(constraint_coeffs, decision_vars)]) == constraint_constants[i]
 
-    # Resolver
     problem.solve()
 
-    # Solucion
     solution = {'vars': [], 'optimal_value': pulp.value(problem.objective)}
     for var in problem.variables():
         solution['vars'].append({var.name: var.varValue})
